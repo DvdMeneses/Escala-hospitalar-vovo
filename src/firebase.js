a7e3331ff098e03f9493a1d50abf743496adab2c
David <david.meneses.cc@gmail.com>
2025-06-20 22:07:45 -0300

    fix: ajusta env

--- src/firebase.js
+++ src/firebase.js
@@ -1,23 +1,17 @@
-// src/firebase.js
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
-
-// Sua configuração Firebase (a que você colou)
 const firebaseConfig = {
-    apiKey: "AIzaSyCiAn2rLz396Rto1U8dMLitlfu7OIt2vh0",
-    authDomain: "escala-hospitalar-da-vovo.firebaseapp.com",
-    databaseURL: "https://escala-hospitalar-da-vovo-default-rtdb.firebaseio.com",
-    projectId: "escala-hospitalar-da-vovo",
-    storageBucket: "escala-hospitalar-da-vovo.firebasestorage.app",
-    messagingSenderId: "279474719875",
-    appId: "1:279474719875:web:d74e82870267d351655fb6",
-    measurementId: "G-NMCRGH43BQ"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
-// Inicializa o app Firebase
 const app = initializeApp(firebaseConfig);
-
-// Exporta o database para usar no app
 const database = getDatabase(app);
 
 export { database };
